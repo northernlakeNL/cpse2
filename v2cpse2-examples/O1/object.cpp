0e464cfc0f159e99b6f7a5d4312a2136aa7e35fc
Northie <tomnoordermeer@live.nl>
2024-02-15 00:08:30 +0100

    remove old version

--- v2cpse2-examples/O1/object.cpp
+++
@@ -1,9 +0,0 @@
-#include "Object.hpp"
-
-void Object::move(sf::Vector2f delta){
-    position+=delta;
-}
-
-sf::FloatRect Object::getBoundingBox() const {
-    return sf::FloatRect(position, sf::Vector2f(0, 0));
-}
